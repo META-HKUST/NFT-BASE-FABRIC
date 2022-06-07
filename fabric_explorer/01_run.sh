ff2bfb37ae6de9c8e0ed151626d508d0966ec38f
Unifiter <unifit_ust@163.com>
2022-06-07 13:29:15 +0800

    Update

--- fabric_explorer/01_run.sh
+++ fabric_explorer/01_run.sh
@@ -2,27 +2,27 @@ FABRIC_CODE=/home/fabric_release/01_Fabric/NFT-BASE-FABRIC
 FABRIC_ENV=~/01_Fabric/hyperledger/
 
 cd $FABRIC_ENV/
-rm fabric_exlporer -r
-mkdir fabric_exlporer
-cd fabric_exlporer
+rm fabric_explorer -r
+mkdir fabric_explorer
+cd fabric_explorer
 
 rm crypto-config -r
 cp -r $FABRIC_ENV/crypto-config/ .
-cd crypto-config/peerOrganizations/org1.unifit.com/users/fabric_exlporer.org1.unifit.com/msp/keystore/
+cd crypto-config/peerOrganizations/org1.unifit.com/users/fabric_explorer.org1.unifit.com/msp/keystore/
 sk=`ls .`
 cp $sk priv_sk
 cd -
 
-cd crypto-config/peerOrganizations/org2.unifit.com/users/fabric_exlporer.org2.unifit.com/msp/keystore/
+cd crypto-config/peerOrganizations/org2.unifit.com/users/fabric_explorer.org2.unifit.com/msp/keystore/
 sk=`ls .`
 cp $sk priv_sk
 cd -
 
 mkdir walletstore
 mkdir pgdata
-cp -r $FABRIC_CODE/fabric_exlporer/connection-profile/ .
-cp $FABRIC_CODE/fabric_exlporer/docker-compose.yaml .
-cp $FABRIC_CODE/fabric_exlporer/config.json .
+cp -r $FABRIC_CODE/fabric_explorer/connection-profile/ .
+cp $FABRIC_CODE/fabric_explorer/docker-compose.yaml .
+cp $FABRIC_CODE/fabric_explorer/config.json .
 
 docker stop explorerdb.mynetwork.com explorer.mynetwork.com
 docker rm explorerdb.mynetwork.com explorer.mynetwork.com
